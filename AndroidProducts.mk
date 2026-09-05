516d31c774a466a76ea847a2bef94f89cd2a15b8
tuananhcn <nguyenanhtuan225tn@gmailc.om>
2026-09-05 13:27:59 +0700

    refactor: update device path inheritance and CI workflow defaults for ossi build

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,7 +8,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_ossi.mk
 
-COMMON_LUNCH_CHOICES := \
-    twrp_ossi-user \
-    twrp_ossi-userdebug \
-    twrp_ossi-eng
+# COMMON_LUNCH_CHOICES := \
+#     twrp_ossi-user \
+#     twrp_ossi-userdebug \
+#     twrp_ossi-eng
--- twrp_ossi.mk
+++ twrp_ossi.mk
@@ -5,15 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
 # Inherit from ossi device
-$(call inherit-product, device/oplus/ossi/device.mk)
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
 
 PRODUCT_DEVICE := ossi
 PRODUCT_NAME := twrp_ossi
