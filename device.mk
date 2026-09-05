a0ccb776b64535d1b6676929b9dee9d26e427a5a
tuananhcn <99953080+tuananhcn@users.noreply.github.com>
2026-09-05 21:37:50 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/oplus/ossi
+DEVICE_PATH := device/oplus/ossi
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
