09fe0720377cd743d7c6ec2f1f726e6115c66c84
tuananhcn <99953080+tuananhcn@users.noreply.github.com>
2026-09-10 16:36:40 +0700

    Update BoardConfig.mk old ossi branch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,29 +1,30 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# #
+# # Copyright (C) 2026 The Android Open Source Project
+# # Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+# #
+# # SPDX-License-Identifier: Apache-2.0
+# #
 
-DEVICE_PATH := device/oplus/ossi
+# # Init
+TARGET_INIT_VENDOR_LIB          := //$(DEVICE_PATH):libinit_oplus_ossi
+TARGET_RECOVERY_DEVICE_MODULES  := libinit_oplus_ossi
 
 # Building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES                      := true
 BUILD_BROKEN_DUP_RULES                          := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES    := true
+
 BUILD_BROKEN_NINJA_USES_ENV_VARS    += RTIC_MPGEN
 BUILD_BROKEN_PLUGIN_VALIDATION      := soong-libaosprecovery_defaults soong-libguitwrp_defaults soong-libminuitwrp_defaults soong-vold_defaults
 
-# File systems
-# TARGET_USERIMAGES_USE_F2FS := true
-# TW_USE_DMCTL               := true
-
-# Init
-TARGET_INIT_VENDOR_LIB          := //$(DEVICE_PATH):libinit_oplus_ossi
-TARGET_RECOVERY_DEVICE_MODULES  := libinit_oplus_ossi
+# Architecture
+TARGET_ARCH                 := arm64
+TARGET_ARCH_VARIANT         := armv8-a
+TARGET_CPU_ABI              := arm64-v8a
+TARGET_CPU_VARIANT          := generic
+TARGET_CPU_VARIANT_RUNTIME  := kryo300
 
 # A/B
-# AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS := \
     boot \
     init_boot \
@@ -53,34 +54,28 @@ AB_OTA_PARTITIONS += \
     my_region \
     my_stock
 
-BOARD_USES_RECOVERY_AS_BOOT := false
-
-# Architecture
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
-TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := kryo300
-
-# APEX
-# DEXPREOPT_GENERATE_APEX_IMAGE := true
-
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := kalama
-TARGET_NO_BOOTLOADER := true
+PRODUCT_PLATFORM                := kalama
+TARGET_BOOTLOADER_BOARD_NAME    := kalama
 
 # Crypto
 BOARD_USES_METADATA_PARTITION   := true
 TW_INCLUDE_CRYPTO               := true
 TW_INCLUDE_OMAPI                := true
 
-# Kernel
-# BOARD_BOOTIMG_HEADER_VERSION := 4
-# BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-# BOARD_KERNEL_IMAGE_NAME := Image
-# TARGET_KERNEL_CONFIG := ossi_defconfig
-# TARGET_KERNEL_SOURCE := kernel/oplus/ossi
+# Debug
+# TARGET_USES_LOGD                := true
+# TWRP_INCLUDE_LOGCAT             := true
+# TARGET_RECOVERY_DEVICE_MODULES  += debuggerd
+# TARGET_RECOVERY_DEVICE_MODULES  += strace
+# RECOVERY_BINARY_SOURCE_FILES    += $(TARGET_OUT_EXECUTABLES)/debuggerd
+# RECOVERY_BINARY_SOURCE_FILES    += $(TARGET_OUT_EXECUTABLES)/strace
+
+# File systems
+TARGET_USERIMAGES_USE_F2FS := true
+TW_USE_DMCTL               := true
+
+# Kernel & GKI Header Configuration
 BOARD_KERNEL_IMAGE_NAME     := Image
 BOARD_BOOT_HEADER_VERSION   := 4
 BOARD_KERNEL_PAGESIZE       := 4096
@@ -88,57 +83,26 @@ BOARD_MKBOOTIMG_ARGS        += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS        += --pagesize $(BOARD_KERNEL_PAGESIZE)
 BOARD_RAMDISK_USE_LZ4       := true
 
-# Kernel - prebuilt
-# TARGET_FORCE_PREBUILT_KERNEL := true
-# ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
-# endif
-
 # Partitions
-BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED  := true
-# BOARD_BOOTIMAGE_PARTITION_SIZE := 104857600
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 104857600
-# BOARD_HAS_LARGE_FILESYSTEM := true
-# BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
-# BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-# BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_SUPER_PARTITION_SIZE := 15569256448 # TODO: Fix hardcoded value
-BOARD_SUPER_PARTITION_GROUPS := oplus_dynamic_partitions
-BOARD_OPLUS_DYNAMIC_PARTITIONS_PARTITION_LIST := system system system_ext system_ext product product vendor vendor odm odm my_product my_product my_company my_company my_carrier my_carrier my_region my_region my_bigball my_bigball my_heytap my_heytap my_stock my_stock my_preload my_preload my_manifest my_manifest my_engineering my_engineering
-BOARD_OPLUS_DYNAMIC_PARTITIONS_SIZE := 15565062144 # TODO: Fix hardcoded value
-
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED         := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE          := 0x6400000
+BOARD_SUPER_PARTITION_SIZE                  := 16106127360
+BOARD_SUPER_PARTITION_GROUPS                := qti_dynamic_partitions
+BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product vendor vendor_dlkm odm
+BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST += my_bigball my_carrier my_company my_engineering my_heytap my_manifest my_preload my_product my_region my_stock
+BOARD_QTI_DYNAMIC_PARTITIONS_SIZE           := 16101933056
 BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_ODM             := odm
 TARGET_COPY_OUT_VENDOR          := vendor
 
 # Platform
-TARGET_BOARD_PLATFORM := kalama
-QCOM_BOARD_PLATFORMS += kalama
+TARGET_BOARD_PLATFORM   := sm8550
+QCOM_BOARD_PLATFORMS    += sm8550
 
 # Recovery
-BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
-TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
-# Verified Boot
-BOARD_AVB_ENABLE := true
-
-# Hack: prevent anti rollback
-PLATFORM_VERSION                := 99.87.36
-PLATFORM_VERSION_LAST_STABLE    := $(PLATFORM_VERSION)
-PLATFORM_SECURITY_PATCH         := 2099-12-31
-VENDOR_SECURITY_PATCH           := $(PLATFORM_SECURITY_PATCH)
-TW_DEVICE_VERSION               := OPLUS-OSSI
-
-# TWRP Configuration
-TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
-TW_INCLUDE_REPACKTOOLS := true
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE    := true
+TARGET_RECOVERY_PIXEL_FORMAT                := RGBX_8888
+TW_INCLUDE_FASTBOOTD                        := true
 
 # Tool
 TW_ENABLE_ALL_PARTITION_TOOLS := true
@@ -148,6 +112,14 @@ TW_INCLUDE_RESETPROP          := true
 TW_USE_TOOLBOX                := true
 TW_INCLUDE_ZSTD               := true
 
+# TWRP display
+TW_BRIGHTNESS_PATH      := /sys/class/backlight/panel0-backlight/brightness
+TW_DEFAULT_BRIGHTNESS   := 1000
+TW_FRAMERATE            := 120
+TW_MAX_BRIGHTNESS       := 2047
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_THEME                := portrait_hdpi
+
 # TWRP file system
 RECOVERY_SDCARD_ON_DATA     := true
 TARGET_USES_MKE2FS          := true
@@ -156,6 +128,19 @@ TW_INCLUDE_FUSE_EXFAT       := true
 TW_INCLUDE_FUSE_NTFS        := true
 TW_INCLUDE_NTFS_3G          := true
 TW_NO_EXFAT_FUSE            := true
+
+# Version
+PLATFORM_VERSION                := 99.87.36
+PLATFORM_VERSION_LAST_STABLE    := $(PLATFORM_VERSION)
+PLATFORM_SECURITY_PATCH         := 2099-12-31
+VENDOR_SECURITY_PATCH           := $(PLATFORM_SECURITY_PATCH)
+# TW_DEVICE_VERSION               := OPLUS-ACE2PRO-ossi
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+
+# Vibrator
+TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 
 # Other TWRP Configurations
 TARGET_RECOVERY_QCOM_RTC_FIX            := true
@@ -168,11 +153,3 @@ TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI      := true
 TW_NO_SCREEN_BLANK                      := true
 TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID  := true
 TW_NO_NETWORK                           := true
-TARGET_NO_RECOVERY := false 
-BOARD_BUILD_RECOVERYIMAGE := true
-BOARD_HAS_RECOVERY_DISK := true 
-RECOVERY_RAMDISK_FORCE_SETUP := true
-# BOARD_PREBUILT_VENDOR_DLKMIMAGE := true 
-# TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
-BOARD_USES_GENERIC_KERNEL_IMAGE := true
-# BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
